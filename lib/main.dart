1458667729c058bf330f60029c28bd061ab2990d
eextjcxltuires <pavelbiatsenia@tut.by>
2023-02-02 04:18:04 +0300

    commit.

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
