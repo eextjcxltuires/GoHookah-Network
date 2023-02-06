e27a3b5f11b21013c7ea2bf01b96ead9f1d0eeff
pashabetenya <pavelbiatsenia@gmail.com>
2023-02-06 17:53:18 +0300

    added changes to the login page.

--- lib/ui/pages/authorization/login_page.dart
+++ lib/ui/pages/authorization/login_page.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/authorization_button.dart';
+import 'widgets/continue_text_button.dart';
+import 'widgets/phone_text_form.dart';
+import 'widgets/password_text_form.dart';
+import 'widgets/title_page.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF2B2B2B),
-      body: Center(
-        child: Text('It is Login page.', style: TextStyle(color: Colors.white)),
+    return Scaffold(
+      backgroundColor: const Color(0xFF2B2B2B),
+      appBar: AppBar(
+        title: const TitlePage(title: 'Вход'),
+        centerTitle: true,
+        iconTheme: const IconThemeData(
+          color: Color(0xFFFFC107),
+        ),
+        backgroundColor: const Color(0xFF2B2B2B),
+        elevation: 0,
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 100.0, 16.0, 0),
+            child: Column(
+              children: <Widget>[
+                // Text Form: Phone.
+                const PhoneTextForm(),
+                const SizedBox(height: 10.0),
+
+                // Text Form: Password.
+                const PasswordTextForm(),
+                const SizedBox(height: 25.0),
+
+                // Text Button.
+                ContinueTextButton(
+                  onPressed: () {},
+                  title: 'Забыли пароль?',
+                ),
+              ],
+            ),
+          ),
+
+          // Lower panel description.
+          AuthorizationButton(
+            title1: 'Войти',
+            title2: 'Зарегистрируйтесь!',
+            text: 'Нет аккаунта?',
+            onPressed1: () {},
+            onPressed2: () {},
+          ),
+        ],
       ),
     );
   }
---
+++ lib/ui/pages/authorization/widgets/password_text_form.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class PasswordTextForm extends StatefulWidget {
+  const PasswordTextForm({Key? key}) : super(key: key);
+
+  @override
+  State<PasswordTextForm> createState() => _PasswordTextFormState();
+}
+
+class _PasswordTextFormState extends State<PasswordTextForm> {
+  TextEditingController textController = TextEditingController();
+
+  bool isObscure = true;
+
+  RegExp digitValidator = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$');
+  bool isAWord = true;
+
+  void setValidator(valid) {
+    setState(() {
+      isAWord = valid;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.text,
+      style: const TextStyle(
+        fontFamily: 'SFProTextRegular',
+        color: Color(0xFFF6F7FB),
+        fontSize: 17.0,
+        fontWeight: FontWeight.w400,
+        letterSpacing: -0.41,
+      ),
+      cursorColor: const Color(0xFFF6F7FB),
+      controller: textController,
+      onChanged: (inputValue) {
+        if (inputValue.isEmpty || digitValidator.hasMatch(inputValue)) {
+          setValidator(true);
+        } else {
+          setValidator(false);
+        }
+      },
+      obscureText: isObscure,
+      decoration: InputDecoration(
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF767676)),
+        ),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF767676)),
+        ),
+        errorBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFD10000)),
+        ),
+        focusedErrorBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFD10000)),
+        ),
+        border: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF767676)),
+        ),
+        labelText: 'Пароль',
+        labelStyle: const TextStyle(
+          fontFamily: 'SFProTextRegular',
+          color: Color(0xFFF6F7FB),
+          fontSize: 17.0,
+          fontWeight: FontWeight.w400,
+          letterSpacing: -0.41,
+        ),
+        errorText: isAWord ? null : 'Пароль слишком лёгкий.',
+        errorStyle: const TextStyle(
+          fontFamily: 'SFProTextRegular',
+          color: Color(0xFFD10000),
+          fontSize: 17.0,
+          fontWeight: FontWeight.w400,
+          letterSpacing: -0.41,
+        ),
+        suffixIcon: IconButton(
+          icon: isObscure
+              ? const Icon(
+                  Icons.visibility_off,
+                  color: Color(0xFF767676),
+                  size: 18.6,
+                )
+              : const Icon(
+                  Icons.visibility,
+                  color: Color(0xFF767676),
+                  size: 18.6,
+                ),
+          onPressed: () {
+            setState(() {
+              isObscure = !isObscure;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
