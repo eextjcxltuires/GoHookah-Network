0f86cb5de7439997dd1b635ac200d6f804af7fc9
pashabetenya <pavelbiatsenia@gmail.com>
2023-02-08 03:02:01 +0300

    started building a home page.

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -5,10 +5,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF2B2B2B),
-      body: Center(
-        child: Text('It is Home page.', style: TextStyle(color: Colors.white)),
+    return Scaffold(
+      backgroundColor: const Color(0xFF2B2B2B),
+      body: Column(
+        children: const <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 60.0, 16.0, 0),
+            child: Text(
+              'Главная',
+              style: TextStyle(
+                fontFamily: 'SFProDisplayBold',
+                color: Color(0xFFF6F7FB),
+                fontSize: 34.0,
+                fontWeight: FontWeight.w700,
+                letterSpacing: 0.37,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
